ff1ff5d0364dc357857fdf0dd8b22a549a3904b0
Gonzalo Marcote <gonzalo.marcote@accenture.com>
2020-06-05 16:08:14 +0200

    Removing first character of env var with `str(env[1:])`

--- tools/vaultValues.py
+++ tools/vaultValues.py
@@ -42,7 +42,7 @@ def lookup(d, pat, rep, path=[]):
 # Function retrieve_vault() to retrieve "vault" values in Hashicorp Vault
 def retrieve_vault(key):
     read_response = client.secrets.kv.v1.read_secret( 
-        path = str(env) + "/" + str(key),
+        path = str(env[1:]) + "/" + str(key),
         mount_point = "phono",
     )
     val = read_response['data'][key]
