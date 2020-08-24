2d352b95932c6481f89341b5108af1f0ce2b171e
Gonzalo Marcote <gonzalo.marcote@accenture.com>
2020-08-24 18:18:18 +0200

    Adding volumes

--- broker/docker-entrypoint.sh
+++ broker/docker-entrypoint.sh
@@ -1,12 +1,5 @@
-#!/bin/bash
+#!/bin/sh
+
 set -e
 
-docker_set_permissions() {
-	local user; user="$(id -u)"
-
-	if [ "$user" = '0' ]; then
-		chown -R mosquitto:mosquitto /etc/mosquitto
-	fi
-}
-
-docker_set_permissions()
+exec "$@"
